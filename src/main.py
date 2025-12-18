4a1201603de8e26935fd270c3ac8351359348e70
Bohdan <x0ryz@proton.me>
2025-12-19 00:06:45 +0200

    Implement WhatsApp messaging feature with Redis integration and update requirements

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from sqladmin import Admin
 from src.database import engine
 from src.admin import LeadAdmin
 
+from faststream.redis import RedisBroker
+
 app = FastAPI()
 admin = Admin(app, engine, title="My Admin")
 
@@ -20,3 +22,16 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/send_message/{phone}")
+async def send_message(phone: str):
+    broker = RedisBroker("redis://redis:6379")
+    await broker.connect()
+
+    await broker.publish(
+        {"phone": phone},
+        channel="whatsapp_messages"
+    )
+
+    print("Завдання відправлено в чергу!")
+    await broker.close()
--- src/worker.py
+++ src/worker.py
@@ -1,6 +1,43 @@
+import httpx, asyncio
+
 from faststream import FastStream
 from faststream.redis import RedisBroker
+from src.schemas import WhatsAppMessage
 from src.config import settings
 
 broker = RedisBroker(settings.REDIS_URL)
 app = FastStream(broker)
+
+async def send_whatsapp_message(phone: str):
+    url = f"{settings.META_URL}/{settings.META_PHONE_ID}/messages"
+    headers = {
+        "Authorization": f"Bearer {settings.META_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": phone,
+        "type": "template",
+        "template": {
+            "name": "hello_world",
+            "language": {
+                "code": "en_US"
+            }
+        }
+    }
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url, headers=headers, json=payload, timeout=10)
+        resp.raise_for_status()
+        return resp.json()
+
+@broker.subscriber("whatsapp_messages")
+async def handle_messages(message: WhatsAppMessage):
+    try:
+        result = await send_whatsapp_message(message.phone)
+        print(f"Message sent to {message.phone}: {result}")
+        await asyncio.sleep(1)
+    except Exception as e:
+        print(f"Failed to send message to {message.phone}: {e}")
+        
