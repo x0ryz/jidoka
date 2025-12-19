22ac010710467a44786e783756903a85ac3246f4
Bohdan <x0ryz@proton.me>
2025-12-19 12:06:25 +0200

    Refactor WhatsApp message handling to initialize and close HTTPX client on app startup and shutdown; pass client to message sending function

--- src/worker.py
+++ src/worker.py
@@ -1,8 +1,9 @@
 import httpx, asyncio
 
-from faststream import FastStream
+from faststream import FastStream, Context, ContextRepo
 from faststream.redis import RedisBroker
 from src.schemas import WhatsAppMessage
+
 from src.logger import setup_logging
 from src.config import settings
 
@@ -11,12 +12,30 @@ logger = setup_logging()
 broker = RedisBroker(settings.REDIS_URL)
 app = FastStream(broker)
 
-async def send_whatsapp_message(phone: str):
+
+@app.on_startup
+async def setup_http_client(context: ContextRepo):
+    client = httpx.AsyncClient(
+        timeout=10.0,
+        headers={
+            "Authorization": f"Bearer {settings.META_TOKEN}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    context.set_global("http_client", client)
+    logger.info("HTTPX Client initialized")
+
+
+@app.after_shutdown
+async def close_http_client(context: ContextRepo):
+    client = context.get("http_client")
+    if client:
+        await client.aclose()
+        logger.info("HTTPX Client closed")
+
+async def send_whatsapp_message(phone: str, client: httpx.AsyncClient):
     url = f"{settings.META_URL}/{settings.META_PHONE_ID}/messages"
-    headers = {
-        "Authorization": f"Bearer {settings.META_TOKEN}",
-        "Content-Type": "application/json"
-    }
 
     payload = {
         "messaging_product": "whatsapp",
@@ -30,18 +49,17 @@ async def send_whatsapp_message(phone: str):
         }
     }
 
-    async with httpx.AsyncClient() as client:
-        resp = await client.post(url, headers=headers, json=payload, timeout=10)
-        resp.raise_for_status()
-        return resp.json()
+    resp = await client.post(url, json=payload)
+    resp.raise_for_status()
+    return resp.json()
 
 @broker.subscriber("whatsapp_messages")
-async def handle_messages(message: WhatsAppMessage):
+async def handle_messages(message: WhatsAppMessage, client: httpx.AsyncClient = Context("http_client")):
     with logger.contextualize(request_id=message.request_id):
         logger.info(f"Received message request for phone: {message.phone}")
 
         try:
-            result = await send_whatsapp_message(message.phone)
+            result = await send_whatsapp_message(message.phone, client)
             logger.success(
                 f"Message sent successfully. Meta Response: {result}")
             await asyncio.sleep(1)
