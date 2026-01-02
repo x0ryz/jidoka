5e206418e9619bc52bc4e912713b32b641bb5696
Bohdan Barulin <x0ryz@proton.me>
2026-01-02 12:34:40 +0200

    dev(frontend): fix proxy and docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: ["dev.x0ryz.cc"],
+    proxy: {
+      "/api": {
+        target: "http://backend:8000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      "/ws": {
+        target: "ws://backend:8000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
